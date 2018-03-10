f15738800cec49fe942abdd8ea14193f4016184a
Alexander Villegas <villegassanchezjhonalexander@gmail.com>
2018-03-09 23:21:03 -0500

    Registro de usuario

--- refugioanimales2/urls.py
+++ refugioanimales2/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^mascota/', include ('apps.mascota.urls')),
     url(r'^adopcion/', include ('apps.adopcion.urls')),
+    url(r'^usuario/', include ('apps.usuario.urls')),
 ]
